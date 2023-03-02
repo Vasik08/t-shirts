1c890befcd1e6c4679a9923ed719d33a13f09255
vasilisa <prosto_vasia@outlook.com>
2023-03-02 19:37:05 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("hello world")
+function vasilisa(){
+    console.log( "vasilisa")
+}
+vasilisa()
+function vidget(){
+    var telegram="telegram: vsss_sim"
+    let tel="tel:89226150046"
+    alert(telegram+"\n"+tel)
+}
+// function age(){
+//     let name = prompt('Введите ваше имя')
+//     alert( 'Ваше имя ' + name + '\n' + 'ВАШЕ ИМЯ ' + name.toUpperCase() + '\n' + 'ваше имя ' + name.toLowerCase())
+// }
+// age()
+// function age(){
+//     let year = prompt('Введите ваш год рождения')
+//     let age = 2022 - year
+//     alert('Вы родились в ' + year + ' году' + '\n' + 'вам ' + age + ' лет')
+// }
+// age()
+ let button = document.getElementById('test')
+ console.log(button)
+ button.style.backgroundColor = 'red'
+function mynumber(){
+    let number = prompt('введи любимое число')
+    let myfav = 4 
+    if(myfav==number){
+        console.log('ты угадал')
+    }
+    else if(myfav!=number){
+        console.log('ты не угадал')
+    }
+
+    
+}
+mynumber()
+for(let i = 1;i<100;i++){
+    console.log(i)
+}
+function showModel() {
+let model = document.getElementById('model');
+model.style.display = 'block' ;
+}
+alert()
+ 
+
