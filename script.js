026bc248cb73ea66a856deb090ff9ed2eece66d8
vasilisa <prosto_vasia@outlook.com>
2023-05-17 17:43:04 +0500

    project finished

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function vidget(){
 //     alert('Вы родились в ' + year + ' году' + '\n' + 'вам ' + age + ' лет')
 // }
 // age()
- let button = document.getElementById('test')
- console.log(button)
- button.style.backgroundColor = 'red'
+let button = document.getElementById('test')
+console.log(button)
+button.style.backgroundColor = 'red'
 function mynumber(){
     let number = prompt('введи любимое число')
     let myfav = 4 
@@ -34,14 +34,96 @@ function mynumber(){
 
     
 }
-mynumber()
+// mynumber()
 for(let i = 1;i<100;i++){
     console.log(i)
 }
 function showModel() {
 let model = document.getElementById('model');
-model.style.display = 'block' ;
+if(model.style.display === 'none'){
+    model.style.display = 'block' 
 }
-alert()
- 
+else {
+    model.style.display = 'none'
+}
+}
+function fillStars(n){
+    let stars = document.getElementsByClassName('star');
+ for(let i = 0; i < n; i++){
+    stars[i].src = "img/star.png"
+ }
+ for(let i = 4; i >= n; i--){
+    stars[i].src = "img/star1.png"
+ }
+}
+function showPodarok(){
+    let pod = document.getElementById('pod');
 
+if(pod.style.display === 'block'){
+    pod.style.display = 'none' 
+}
+else {
+    pod.style.display = 'block'
+}
+}
+
+setInterval(showPodarok ,3000);
+
+function CheckInput(){
+let phone = document.getElementById('phone');
+console.log(phone.value)
+let pattern = RegExp(/\d/ , 'gi');
+if (phone.value.match(pattern))
+
+if (phone.value.length == 10) {
+    console.log('Длинна правильная');
+    phone.style.border = "5px solid green";
+
+    
+} else {
+    phone.style.border = "5px solid red"
+}
+}
+function checkName(){
+let name = document.getElementById('name');
+console.log(name.value)
+if(name.value === ''){
+    alert('ошибка')
+}
+}
+function chargeTshirts(dir){
+    let futbolkas = ['img/depositphotos_74690493-stock-photo-blank-black-t-shirt.jpg' , 'img/foot.jpg', 'img/photo.png', 'img/photo_1.png', 'img/footbolka.jpg', 'img/footb1.jpg', 'img/foot5.png']
+
+let imgL = document.getElementById('imgL');
+let imgR = document.getElementById('imgR');
+
+let t = imgL.src.lastIndexOf('img');
+let sub = imgL.src.slice(t);
+
+let idL = futbolkas.indexOf(sub);
+
+if(dir === 'r'){
+    if(idL === 0){
+        idL = futbolkas.length - 1
+    }
+    else{
+        idL--
+    }
+    
+}
+else if (dir === 'l'){
+    if(idL === futbolkas.length - 1){
+        idL = 0 
+    }
+    else{
+        idL++
+    }
+}
+let idR = idL + 1;
+    if(idR === futbolkas.length){
+        idR = 0 
+    }
+
+imgL.src = futbolkas[idL]
+imgR.src = futbolkas[idR]
+}
